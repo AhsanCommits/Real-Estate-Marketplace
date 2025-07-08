f8ef3ddce763cb8b91eec7ac7abdf45223264bf7
Ch Ahsan  Fatima <ahsanraza00059@gmail.com>
2025-07-08 15:01:23 +0500

    (chore):  install react js and tailwind css and create the first template

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {  
+  console.log('Hello world!') 
+  console.log('This is a test log message.')
+  console.log('This is another test log message.')
+  console.log('This is a third test log message.')
+  console.log('This is a fourth test log message.')
+  return (
+    <>
+      <h1 class="text-red-500">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
